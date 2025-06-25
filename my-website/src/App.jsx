82f9275feb81594289efc06074fa65dee8a3d8bf
karson94 <157438223+karson94@users.noreply.github.com>
2025-06-25 06:03:45 -0500

    added pages, basic navbar, messed with hover colors, added resume pdf to resume page

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -1,35 +1,18 @@
 import { Flex, Box, Heading, Text, Button, Stack } from "@chakra-ui/react";
+import {Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Resume from "./pages/Resume"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <Flex
-      minH="100vh"
-      w="100vw"
-      align="center"
-      justify="center"
-      bg="gray.900"
-    >
-      <Box
-        textAlign="center"
-        p={8}
-        borderRadius="md"
-        boxShadow="md"
-      >
-        <Heading as="h1" size="2xl" mb={4} color="whiteAlpha.800">
-          Sam Karson
-        </Heading>
-
-        <Text fontSize="xl" mb={8} color="gray.700">
-          Welcome to my professional portfolio website!
-        </Text>
-
-        <Stack spacing={4} align="center">
-          <Button colorScheme="teal" size="lg" bg="" >View My Projects</Button>
-          <Button colorScheme="teal" size="lg">Contact Me</Button>
-        </Stack>
-
-      </Box>
-    </Flex>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path ="/" element={<Home />} />
+      <Route path ="/resume" element={<Resume />} />
+    </Routes>
+    </>
   );
 }
 
--- my-website/src/main.jsx
+++ my-website/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
 import {ChakraProvider} from '@chakra-ui/react'
+import {BrowserRouter} from "react-router-dom"
 import { system } from "@chakra-ui/react/preset";
 
 const root = createRoot(document.getElementById('root')).render(
   <ChakraProvider value={system}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>,
 )
