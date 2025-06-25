39dd9cbee10f23b7270c11bcb0bfd7a2802d7463
karson94 <157438223+karson94@users.noreply.github.com>
2025-06-25 04:54:54 -0500

    init'ing project and basics

---
+++ my-website/src/App.jsx
@@ -0,0 +1,36 @@
+import { Flex, Box, Heading, Text, Button, Stack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Flex
+      minH="100vh"
+      w="100vw"
+      align="center"
+      justify="center"
+      bg="gray.900"
+    >
+      <Box
+        textAlign="center"
+        p={8}
+        borderRadius="md"
+        boxShadow="md"
+      >
+        <Heading as="h1" size="2xl" mb={4} color="whiteAlpha.800">
+          Sam Karson
+        </Heading>
+
+        <Text fontSize="xl" mb={8} color="gray.700">
+          Welcome to my professional portfolio website!
+        </Text>
+
+        <Stack spacing={4} align="center">
+          <Button colorScheme="teal" size="lg" bg="" >View My Projects</Button>
+          <Button colorScheme="teal" size="lg">Contact Me</Button>
+        </Stack>
+
+      </Box>
+    </Flex>
+  );
+}
+
+export default App;
---
+++ my-website/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import './index.css'
+import App from './App.jsx'
+import {ChakraProvider} from '@chakra-ui/react'
+import { system } from "@chakra-ui/react/preset";
+
+const root = createRoot(document.getElementById('root')).render(
+  <ChakraProvider value={system}>
+    <App />
+  </ChakraProvider>,
+)
